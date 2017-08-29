dfbebfffdc8d6a1e61a070ec977d0f585d91759d
Pawan Singh <pawan@Pawans-MacBook-Pro.local>
2017-08-30 00:18:46 +0530

    Made compatible to swift 3

--- NAPPS/NAPPS/AppDelegate.swift
+++ NAPPS/NAPPS/AppDelegate.swift
@@ -9,44 +9,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool {
+    func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
         
         print("*** Received application callback ***")
         print("Application callback URL received: \(url.absoluteString)")
         
         // If the callback was for the OpenID Connect authentication, then handle the response
-        let baseUrl = url.absoluteString.componentsSeparatedByString("?")[0]
-        if baseUrl.lowercaseString == app_redirect_uri.lowercaseString {
+        let baseUrl = url.absoluteString.components(separatedBy: "?")[0]
+        if baseUrl.lowercased() == app_redirect_uri.lowercased() {
             
             // Close the SFSafariViewController
-            window!.rootViewController?.presentedViewController?.dismissViewControllerAnimated(true , completion: nil)
+            window!.rootViewController?.presentedViewController?.dismiss(animated: true , completion: nil)
             
             // Kick off the authentication flow (see ProtocolHelper.swift)
             SessionManager.currentSession.inErrorState = false;
